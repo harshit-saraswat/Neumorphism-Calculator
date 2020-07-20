d4fcfbc25e23fe68e1f1ce65a2f7c96f1a486068
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-07-20 23:38:49 +0530

    Added Minimal JS and JQuery functionality. Added '.' button for floating numbers

--- js/index.js
+++ js/index.js
@@ -1,3 +1,35 @@
-$('.button').on('click', function () {
-    console.log($(this).attr('name'));
-});
+// $('.button').on('click', function () {
+//     console.log($(this).attr('name'));
+// });
+
+function getHistory(){
+    return $('.history-text').text();
+}
+
+function setHistory(values){
+    $('.history-text').text(values);
+}
+
+function getResult(){
+    return $('.result-text').text();
+}
+
+function setResult(values){
+    if(values==""){
+        $('.result-text').text(values);    
+    }else{
+        $('.result-text').text(getFormattedNumber(values));
+    }
+}
+
+function getFormattedNumber(numString){
+    var num=Number(numString);
+    console.log(num)
+    var value=num.toLocaleString("en");
+    return value;
+}
+
+function getReverseFormattedNumber(numString){
+    return Number(numString.replace(/,/g,''));
+}
+
