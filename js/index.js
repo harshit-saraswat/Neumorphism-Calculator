4daad122cd77a261dc57136cd6093db39f227048
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-07-21 23:30:55 +0530

    Done with functionality

--- js/index.js
+++ js/index.js
@@ -1,6 +1,3 @@
-// $('.button').on('click', function () {
-//     console.log($(this).attr('name'));
-// });
 
 function getHistory(){
     return $('.history-text').text();
@@ -18,31 +15,63 @@ function setResult(values){
     if(values==""){
         $('.result-text').text(values);    
     }else{
-        $('.result-text').text(getFormattedNumber(values));
+        $('.result-text').text(values);
+        // $('.result-text').text(getFormattedNumber(values));
     }
 }
 
-function getFormattedNumber(numString){
-    var num=Number(numString);
-    var value=num.toLocaleString("en");
-    return value;
-}
+// function getFormattedNumber(numString){
+//     // var num=Number(numString);
+//     var num=parseFloat(numString);
+//     var value=num.toLocaleString("en");
+//     console.log(value)
+//     return value;
+// }
 
-function getReverseFormattedNumber(numString){
-    return Number(numString.replace(/,/g,''));
-}
+// function getReverseFormattedNumber(numString){
+//     return Number(numString.replace(/,/g,''));
+// }
 
 // Handling Operator Clicks
 $('.operator').on('click',function () {
-    console.log($(this).attr('name'));    
+    switch($(this).attr('name')){
+        case 'C':
+            setHistory("");
+            setResult("");
+            break;
+        case 'CE':
+            var result=getResult();
+            setResult(result.substr(0,result.length-1));
+            break;
+        default:
+            var history=getHistory();
+            var result=getResult();
+            var char=$(this).attr('name');
+            if(result!=""){
+                history+=result;
+                if(char=="="){
+                    result=eval(history);
+                    setResult(result);
+                    setHistory("");
+                }
+                else{
+                    history+=char;
+                    setHistory(history);        
+                    setResult("");
+                }
+            }
+    }
 });
 
 // Handling Number Clicks
 $('.number').on('click',function () {
     
-    var result = getReverseFormattedNumber(getResult());
+    // var result = getReverseFormattedNumber(getResult());
+    var result = getResult();
     if(result!=NaN){
-        result+=$(this).attr('name');
-        setResult(result);
+        if(!($(this).attr('name')=='.' && result.includes('.'))){
+            result+=$(this).attr('name');
+            setResult(result);
+        }
     }
 });
